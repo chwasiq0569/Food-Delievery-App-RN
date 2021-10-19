94f073edc5d62bb246126d5526af98464344be10
chwasiq0569 <wasiqabdullah222@gmail.com>
2021-10-19 11:24:53 +0500

    Developed Header tabs

---
+++ screens/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import HeaderTabs from "./../components/HeaderTabs/HeaderTabs";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <HeaderTabs />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Home;
