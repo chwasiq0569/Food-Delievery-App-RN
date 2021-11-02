63323b2eff7c11e8aa8a2fd61203c3ff545fcc28
chwasiq0569 <wasiqabdullah222@gmail.com>
2021-11-02 10:45:41 +0500

    Developed ViewCart & OrderItem

--- components/retaurantDetails/ViewCart.js
+++ components/retaurantDetails/ViewCart.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { View, StyleSheet, Text, TouchableOpacity, Modal } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
+import OrderItem from "./OrderItem";
 
 const ViewCart = () => {
   const [modalVisible, setModalVisible] = React.useState(false);
 
+  const restaurantName = useSelector(
+    (state) => state.cartReducer.selectedItems.restaurantName
+  );
   const items = useSelector((state) => state.cartReducer.selectedItems.items);
 
   const total = items
@@ -22,26 +26,46 @@ const ViewCart = () => {
 
   const checkoutModalContent = () => {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          marginTop: 30,
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: "black",
-            padding: 10,
-            borderRadius: 30,
-            width: 150,
-            alignItems: "center",
-          }}
-        >
-          <TouchableOpacity onPress={() => setModalVisible(false)}>
-            <Text style={{ color: "white" }}>Checkout</Text>
-          </TouchableOpacity>
+      <View style={styles.modalContainer}>
+        <View style={styles.modalCheckoutContainer}>
+          <Text style={styles.restaurantName}>{restaurantName}</Text>
+          {items.map((item, index) => (
+            <OrderItem key={index} item={item} />
+          ))}
+          <View style={styles.subTotalContainer}>
+            <Text style={styles.subTotalText}>Subtotal</Text>
+            <Text>{totalUSD}</Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              marginTop: "auto",
+            }}
+          >
+            <TouchableOpacity
+              style={{
+                flexDirection: "row",
+                backgroundColor: "black",
+                padding: 10,
+                borderRadius: 30,
+                width: 150,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={{ color: "white", fontSize: 16 }}>Checkout</Text>
+            </TouchableOpacity>
+          </View>
+          {/* <View style={{ flexDirection: "row", justifyContent: "center" }}>
+            <TouchableOpacity
+              style={{ zIndex: 1000 }}
+              onPress={() => setModalVisible(false)}
+            >
+              <Text style={{ color: "white", fontSize: 20 }}>Checkout</Text>
+            </TouchableOpacity>
+          </View> */}
         </View>
       </View>
     );
@@ -114,6 +138,61 @@ const styles = StyleSheet.create({
     width: 300,
     position: "relative",
   },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0, 0, 0, 0.7)",
+  },
+  modalCheckoutContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    height: 500,
+    borderWidth: 1,
+  },
+  restaurantName: {
+    textAlign: "center",
+    fontWeight: "700",
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  subTotalContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 15,
+  },
+
+  subTotalText: {
+    textAlign: "left",
+    fontWeight: "700",
+    fontSize: 15,
+    marginBottom: 10,
+  },
 });
 
 export default ViewCart;
+
+{
+  /* <View
+  style={{
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 30,
+  }}
+>
+  <View
+    style={{
+      backgroundColor: "black",
+      padding: 10,
+      borderRadius: 30,
+      width: 150,
+      alignItems: "center",
+    }}
+  >
+    <TouchableOpacity onPress={() => setModalVisible(false)}>
+      <Text style={{ color: "white" }}>Checkout</Text>
+    </TouchableOpacity>
+  </View>
+</View> */
+}
